9dec21e704e5fbd68c49068db0c0cd3d17eafdb4
Heraldo Arman <88827832+HeraldoArman@users.noreply.github.com>
2024-05-09 21:12:36 +0700

    menambahkan function cetak main menu

--- main.go
+++ main.go
@@ -42,31 +42,20 @@ func print_waktu(){
 
 func input_rentang_waktu() string{
 	var waktu_string string
+	//menginput waktu dalam bentuk string
 	fmt.Scan(&waktu_string)
 	return waktu_string
 }
 
 func cek_rentang_waktu() bool{
-	//var rentang string
-	//var d1, m1, y1, d2, m2, y2, total_waktu_sekarang, total_waktu_1, total_waktu_2 int
-	//rentang = input_rentang_waktu()
-//
-	//now := time.Now()
-	//tahun_sekarang, bulan_sekarang, hari_sekarang := now.Date()
-	//total_waktu_sekarang = 
-//
-//
-	//d1 = int(rentang[0]+rentang[1])
-	//m1 = int(rentang[3]+rentang[4])
-	//y1 = int(rentang[6]+rentang[7]+rentang[8]+rentang[9])
-	//d2 = int(rentang[13]+rentang[14])
-	//m2 = int(rentang[16]+rentang[17])
-	//y2 = int(rentang[19]+rentang[20]+rentang[21]+rentang[22])
 	var rentang1, rentang2 string
 
+	//mendapatkan input waktu
 	fmt.Println("masukkan rentang waktu awal dengan format dd/mm/yyyy")
 	rentang1 = input_rentang_waktu()
+	//menggunakan module time untuk mengubah format penanggalan menjadi bertipe time.time
 	cek_start, err := time.Parse("02/01/2006", rentang1)
+	//mengecek error dalam input
 	if err != nil {
         fmt.Println("Format tanggal awal tidak valid:", err)
     }
@@ -79,8 +68,19 @@ func cek_rentang_waktu() bool{
         fmt.Println("Format tanggal akhir tidak valid:", err)
     }
 
-	hari_ini := time.Now()
 
+	//mengecek tanggal hari ini
+	hari_ini := time.Now()
+	//me return nilai true atau false sesuai dengan rentang waktu
 	return hari_ini.After(cek_start) && hari_ini.Before(cek_akhir)
-	
 }
+
+func cetak_main_menu(){
+	fmt.Println("Selamat datang di aplikasi pemilihan umum")
+	fmt.Println("Silahkan masukkan opsi yang ingin anda pilih")
+	fmt.Println("1. Memilih anggota parlemen")
+	fmt.Println("2. List calon anggota")
+	fmt.Println("3. Pencarian data calon anggota")
+	fmt.Println("4. Exit")
+}
+
