45c090544be09eb0bfe66417ad6c0cb020fc09ad
Heraldo Arman <88827832+HeraldoArman@users.noreply.github.com>
2024-05-09 15:06:02 +0700

    membuat variabel anggota dan pemilih

--- main.go
+++ main.go
@@ -4,7 +4,25 @@ import (
 	"fmt"
 )
 
+
+const NMAX int = 512
+type AnggotaParlemen struct {
+	nama string
+	partai string
+	suara int
+
+}
+
+type PemilihTetap struct {
+	nama string
+	pilihan string
+}
+
+type DaftarCalonAnggotaParlemen [NMAX]AnggotaParlemen
+type DaftarPemilih [NMAX]PemilihTetap
+
+
 func main() {
-	fmt.Println("Hello, World!")
-	
+	var pemilih DaftarPemilih
+	fmt.Println(pemilih[0].nama)
 }
